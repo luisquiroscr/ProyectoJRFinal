fe6d9cb0b182771eff8657b7b43eb49463309585
luisquiroscr <lquiros@vlacademy.com>
2025-02-10 02:39:24 -0600

    Entregable 2 Proyecto
    Se realiza el entregable 2 desarrollado en la clase del 5 de febrero 2025. Se ajustaron algunas cosas respecto a lo que el profesor solicito.

--- JS/PagesJS/Practica_CICLO.js
+++ JS/PagesJS/Practica_CICLO.js
@@ -1,9 +1,112 @@
 console.log(`Funcionando Archivo: Practica_CICLO`);
 let personas = [
-    { nombre: "Juan", edad: 25, activo: true },
-    { nombre: "Ana", edad: 17, activo: false },
-    { nombre: "Carlos", edad: 30, activo: true },
-    { nombre: "Laura", edad: 16, activo: true },
-    { nombre: "Pedro", edad: 40, activo: false }
+    { nombre: "Juan", edad: 25, activo: true },  //array 0
+    { nombre: "Ana", edad: 17, activo: false },  //array 1
+    { nombre: "Carlos", edad: 30, activo: true }, //array 2
+    { nombre: "Laura", edad: 16, activo: true }, //array 3
+    { nombre: "Pedro", edad: 40, activo: false } //array 4
 ];
 
+console.log(personas[4].nombre)
+
+console.log(personas[1].nombre)
+
+//bucles o ciclos
+
+//Foreach
+
+// se reemplaza la palabra array y se coloca el array propio
+//element seria las posiciones o, 1, 2 y todas la as posiciones que tenga
+
+personas.forEach(element => {
+    console.log(element)
+});
+
+let Clientes = [
+    { nombre: "Juan", edad: 25, activo: true },  //array 0
+    { nombre: "Ana", edad: 17, activo: false },  //array 1
+    { nombre: "Carlos", edad: 30, activo: true }, //array 2
+    { nombre: "Laura", edad: 16, activo: true }, //array 3
+    { nombre: "Pedro", edad: 40, activo: false } //array 4
+];
+
+Clientes.forEach(element => {
+    if (element.activo == true){
+        console.log(element.nombre,element.activo)
+    }
+    
+});
+
+//Cantidad de elementos que tiene un array
+console.log(personas.length)
+
+//Filtrar por una propiedad
+let buscarnombre = personas.find(x =>x.nombre === "Ana")  //Esto solo sirve para arreglos
+
+console.log(buscarnombre)
+
+
+let buscaractivo = personas.find(x =>x.activo === true)  //Esto solo sirve para arreglos
+
+console.log(buscaractivo)
+
+function Filtrar() { 
+    let valor = document.getElementById('sensitivo').value;
+    console.log(valor)
+}
+
+//toUpperCase convierte a mayusculas de manera interna
+//trim elimina espacios en blanco
+function buscarDesdeInput() {
+    let input1 = document.getElementById('buscarNombre').value.trim();
+    let resultado = document.getElementById('resultadoBusqueda');
+    
+    //Busca en el arreglo
+    let busqueda = personas.find(x=> x.nombre.toUpperCase() === input1.toUpperCase())
+
+    //Aqui se indica si el resultado es verdadero o falso
+    if (busqueda) {
+        resultado.innerHTML = `La Persona ${busqueda.nombre} ha sido encontrada dentro de la base de datos.`
+        
+    }
+
+    else {
+        resultado.innerHTML = `Persona no encontrada en la base de datos.`
+    }
+}
+
+
+//Crear la funcion del onclick
+//Input id value //no tiene
+//mostrr resultado iiner
+
+function primeraPersonaActiva() {
+    let resultado = document.getElementById('resultadoActivo');
+
+    let busqueda = personas.find(x=> x.activo == true)
+//Aqui le indicamos si el resultado es verdadero o falso
+
+if (busqueda) {
+    resultado.innerHTML = `Primera persona encontrada ${busqueda.nombre}`
+
+}
+
+else {
+    resultado.innerHTML = `No se encontro ninguna persona que cumpla los criterio de la busqueda`
+}
+}
+
+//Caso 6
+
+function personasActivas() {
+    let resultado = document.getElementById('listaActivos');
+
+    personas.forEach(element => {
+        if (element.activo ==true) {
+resultado.innerHTML += `<li>nombre: ${element.nombre}</>`  //El += es para que no borre el anterior y mas bien vaya agregando elementos que cumplan con los criterios 
+        }
+        
+    });
+}
+
+// Entregable 2 hacer ejercicios 3, 4 y 5
--- JS/PagesJS/Practica_Listado_Clientes.js
+++ JS/PagesJS/Practica_Listado_Clientes.js
@@ -1,1 +1,10 @@
 console.log(`Funcionando Archivo:  Practica_Listado_Clientes`);
+
+function exportToExcel() {
+
+    try {
+        let table = ContenedorTabla; // Asegúrate de que tu tabla tenga este ID
+        let table = document.getElementById('ttabla'); // Asegúrate de que tu tabla tenga este ID
+        let wb = XLSX.utils.table_to_book(table);
+        XLSX.writeFile(wb, "empleados.xlsx");
+    } catch (error) }
