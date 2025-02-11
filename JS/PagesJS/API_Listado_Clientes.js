a06636f78b96da6f404f8159b44e34461fd7ea1f
luisquiroscr <lquiros@vlacademy.com>
2025-02-10 20:40:08 -0600

    Error mostrar en tabla
    Se muestra en la consola pero no en la tabla

--- JS/PagesJS/API_Listado_Clientes.js
+++ JS/PagesJS/API_Listado_Clientes.js
@@ -4,49 +4,59 @@ const Api_Usuarios = "https://jsonplaceholder.typicode.com/users"
 
 
 // Datos locales simulando la API
-let Datos_Locales = [];
-
-
 
 
 
 let DatosFiltrados = []
 let contenedor = document.getElementById('tbody');
 
-DatosFiltrados = Datos_Locales
+
+//aela Visualizar al ejecutar la pagina DOM
 document.addEventListener('DOMContentLoaded', () => {
-    ConsultarDatos()
+    consultarDatos()
+
 });
 
-function ConsultarDatos() {
-    fetch(Api_Usuarios)
-        
-        .then(response => response.json())
-        .then(result => {
-            // custom error
-            console.log(result)
-        })
-        .catch(error => {
-            // common error
-            return null;
-        });
-}
-
-
-//ConsultaAPIClientes
-// Consultar a la api Api_Usiarios
-
+// Consultar Datos del API mediante Fetch(AJAX) AXIOS
 function consultarDatos() {
-    fetch(Api_Usuarios)
+
+    //fetchget
+    fetch(Api_Usuarios) //1- se le indica la url de la api 
         
-        .then(response => response.json())
-        .then(result => {
+        .then(response => response.json()) // 2 -Conversion de datos , JSON 
+        .then(result => {           //3- Resultado  result
             // custom error
             console.log(result)
+            DatosFiltrados = result
+            Mostrar_CampoTabla()
         })
-        .catch(error => {
+        .catch(error => {      ///4- Catch
             // common error
+            alert("Error ",error)
             return null;
         });
     
+}
+
+function Mostrar_CampoTabla() {
+    
+    let html = ""
+    
+    DatosFiltrados.forEach(element => {
+        
+        console.log(element)
+
+        html += `
+        
+         <tr>
+            <td> ${element.id} </td>
+            <td> ${element.name} </td>
+            <td> ${element.email} </td>
+        
+        </tr>
+        
+        
+        `
+
+    });
 }
